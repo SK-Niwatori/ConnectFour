47fee56b7e95404cc35303892f18ec924d2a0166
SK-Niwatori <ksksagawa123@gmail.com>
2026-07-01 02:57:41 +0900

    ゲーム状態管理のためのセレクタ関数を追加

--- game-state.rkt
+++ game-state.rkt
@@ -4,6 +4,13 @@
 
 ;;;; 状態の管理
 
+(provide world)
+(provide world-board
+         world-turn
+         world-selected-column
+         world-winner
+         world-scene)
+
 ;; ゲーム全体の状態
 (define (world board           ;; 現在の盤面を表す2次元リスト
                turn            ;; 現在どちらのプレイヤーの番かを表す ('red, 'yellow)
@@ -12,3 +19,10 @@
                scene           ;; 現在の画面 ('title, 'playing, 'result)
                )
   (list board turn selected-column winner scene))
+
+;; 状態のセレクタ
+(define (world-board w) (car w))
+(define (world-turn w) (cadr w))
+(define (world-selected-column w) (caddr w))
+(define (world-winner w) (cadddr w))
+(define (world-scene w) (car (cddddr w)))
