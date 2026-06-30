1b2be6c4f58f1080959f4ef167c7d48e90879e8d
SK-Niwatori <ksksagawa123@gmail.com>
2026-07-01 02:39:00 +0900

    ゲームの起動処理を追加

--- main.rkt
+++ main.rkt
@@ -1,3 +1,11 @@
 #lang racket
 (require 2htdp/image)
 (require 2htdp/universe)
+
+;;;; ゲームの起動
+
+;; アプリケーションの実行を開始
+(define (start)
+  (big-bang (world (make-empty-board) 'red 0 #f 'title)
+            (to-draw draw-scene)
+            (on-key control)))
