3f35428ca60e88721b41640400e20387226b5c42
SK-Niwatori <ksksagawa123@gmail.com>
2026-07-01 21:58:44 +0900

    勝利判定関数check-winnerを追加し、引数および戻り値に関するコメントを追加

--- judge.rkt
+++ judge.rkt
@@ -4,3 +4,16 @@
 
 ;;;; 勝利判定の処理
 
+(provide check-winner)
+
+;; 縦横斜めで4つ並んでいるかの判定
+(define (check-winner board column)
+  ;; 引数:
+  ;;   board  : コマを落とした後の盤面を表す2次元リスト
+  ;;   column : コマを落とした列 (0 ~ 6)
+  ;;
+  ;; 戻り値:
+  ;;   判定結果 (#f, 'red, 'yellow, 'draw)
+  
+  ;; TODO
+  (error "未実装"))
