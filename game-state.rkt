c48e0de96b2e8c70bbba7aa9ad0114c62677d7a2
SK-Niwatori <ksksagawa123@gmail.com>
2026-07-01 20:38:19 +0900

    プレイヤーのターンを交代する関数を追加し、引数および戻り値に関するコメントを追加

--- game-state.rkt
+++ game-state.rkt
@@ -27,10 +27,24 @@
 (define (world-winner w)          (cadddr w))
 (define (world-scene w)           (car (cddddr w)))
 
+;; プレイヤーのターンを交代
+(define (switch-turn turn)
+  ;; 引数:
+  ;;   turn : 現在どちらのプレイヤーの番かを表す ('red, 'yellow)
+  ;;
+  ;; 戻り値
+  ;;   交代した後のプレイヤーのシンボル
+
+  ;; TODO
+  (error "未実装"))
+
 ;; 現在選択している列を左へ1つ移動
 (define (move-column-left column)
   ;; 引数:
   ;;   column : 現在選択している列 (0 ~ 6)
+  ;;
+  ;; 戻り値:
+  ;;   列を左へ1つ移動させた後の列の番号
   
   ;; TODO
   (error "未実装"))
@@ -39,6 +53,22 @@
 (define (move-column-right column)
   ;; 引数:
   ;;   column : 現在選択している列 (0 ~ 6)
+  ;;
+  ;; 戻り値:
+  ;;   列を右へ1つ移動させた後の列の番号
+
+  ;; TODO
+  (error "未実装"))
+
+;; コマを落とす
+(define (drop-piece board column piece)
+  ;; 引数:
+  ;;   board  : 現在の盤面を表す2次元リスト
+  ;;   column : コマを落とす列 (0 ~ 6)
+  ;;   piece  : 落とすコマの種類 ('red, 'yellow)
+  ;;
+  ;; 戻り値:
+  ;;   コマを落とした後の盤面を表す2次元リスト
 
   ;; TODO
   (error "未実装"))
