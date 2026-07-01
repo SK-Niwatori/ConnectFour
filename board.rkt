4eed287b9895e3e1025c59edf9603271f565f51d
SK-Niwatori <ksksagawa123@gmail.com>
2026-07-01 22:47:58 +0900

    column-full?関数を追加し、引数および戻り値に関するコメントを追加

--- board.rkt
+++ board.rkt
@@ -5,6 +5,7 @@
 ;;;; 盤面の管理
 
 (provide make-empty-board)
+(provide column-full?)
 
 ;; 空の盤面の作成
 (define (make-empty-board)
@@ -15,3 +16,15 @@
     (empty empty empty empty empty empty)
     (empty empty empty empty empty empty)
     (empty empty empty empty empty empty)))
+
+;; 指定された列が埋まっているか確認
+(define (column-full? board column)
+  ;; 引数:
+  ;;   board  : 現在の盤面を表す2次元リスト
+  ;;   column : 現在選択している列 (0 ~ 6)
+  ;;
+  ;; 戻り値:
+  ;;   真偽値 (#t, #f)
+
+  ;; TODO
+  (error "未実装"))
