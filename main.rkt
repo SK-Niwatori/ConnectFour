2eb0e9e56c732d711b0a6832836b9d2c31d0f690
SK-Niwatori <ksksagawa123@gmail.com>
2026-07-01 23:31:42 +0900

    start手続きを削除し、big-bangを直接呼び出すように変更

--- main.rkt
+++ main.rkt
@@ -9,7 +9,6 @@
 ;;;; ゲームの起動
 
 ;; アプリケーションの実行を開始
-(define (start)
-  (big-bang (world (make-empty-board) 'red 0 #f 'title)
-            (to-draw draw-scene)
-            (on-key control)))
+(big-bang (world (make-empty-board) 'red 0 #f 'title)
+          (to-draw draw-scene)
+          (on-key control))
