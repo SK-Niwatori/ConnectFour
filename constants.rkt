8a149cc82488de568d508261817b961587325f1b
SK-Niwatori <ksksagawa123@gmail.com>
2026-07-02 11:48:33 +0900

    constants.rktにコマのサイズを定義し、関連する定数を整理

--- constants.rkt
+++ constants.rkt
@@ -8,5 +8,17 @@
 ;; 画面のサイズ
 (define SCENE-SIZE 800)
 
+;; コマのサイズ
+(define PIECE-SIZE 80)
+
 ;; ゲーム・リザルト画面の背景
 (define SCENE-BACK (empty-scene SCENE-SIZE SCENE-SIZE "Dodger Blue"))
+
+;; 空（白色）のコマ
+(define PIECE-EMPTY (circle PIECE-SIZE "solid" "white"))
+
+;; 赤色のコマ
+(define PIECE-RED (circle PIECE-SIZE "solid" "red"))
+
+;; 黄色のコマ
+(define PIECE-YELLOW (circle PIECE-SIZE "solid" "yellow"))
